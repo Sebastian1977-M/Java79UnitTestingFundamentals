57c1b85b99e4f8a7e66aaf0a135b43a03719c028
Sebastian1977-M <sebastianmata1977@yahoo.com>
2025-04-08 18:44:14 +0300

    Second commit

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,25 @@
 package calculator;
 
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+//adunarea ar trebui sa fie corecta pt nr intregi;
+@Test
+    public void testAdunare () {
+    System.out.println("S-a intrat in testul pt. verificarea adunarii");
+    double rezultatActual = calculator.adunare(4,   5);
+
+    //definiti rezultatul actual asteptat -> double rezultatAsteptat  = 9;
+    assertEquals ( 9,rezultatActual);
+
+}
+@Test
+    public void testScadere() {
+
+}
 }
