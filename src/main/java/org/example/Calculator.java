b1d7fa4c15415cb90ed38b6717e79420d74ab4fb
Sebastian1977-M <sebastianmata1977@yahoo.com>
2025-04-08 18:42:09 +0300

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Calculator {
+
+    public double adunare (double a,double b) {
+        return a + b;
+
+    }
+    public double ascadere  (double a,double b) {
+        return a - b;
+
+    }
+    public double inmultire (double a,double b) {
+        return a * b;
+
+    }
+    public double impartire  (double a,double b) {
+        if (b==0) {
+
+            throw new ArithmeticException("Nu se poate face diviziunea la 0!");
+
+        }
+        return a / b;
+    }
+}
